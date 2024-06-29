ce6093c84b6942b7173c4153a2b97242504b4dd1
ChanGyunLee <lgyn10@naver.com>
2024-06-29 22:03:37 +0900

    env: '@typescript-eslint/no-unused-vars': 'warn' 항목 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,15 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  env: { browser: true, es2020: true, webextensions: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': 'warn',
       { allowConstantExport: true },
     ],
   },
 }
+};
