e387e5f48394c5743e65f7a69bc10683954f1fcc
ChanGyunLee <lgyn10@naver.com>
2024-06-29 22:10:43 +0900

    env: 크롬 확장프로그램 개발 환경 조성

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       index: resolve(__dirname, 'index.html'),
+  //       popup: resolve(__dirname, 'public/popup.html'),
+  //       background: resolve(__dirname, 'public/background.ts'),
+  //     },
+  //     output: {
+  //       entryFileNames: '[name].js',
+  //       chunkFileNames: '[name].js',
+  //       assetFileNames: '[name].[ext]',
+  //     },
+  //   },
+  // },
+});
