84b74fc097a8b3537aa629b318f9f173fe412f68
ChanGyunLee <lgyn10@naver.com>
2024-07-02 17:11:55 +0900

    feat: mui 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Card } from '@mui/material';
 import './App.css';
 import Test from './Test';
 
 function App() {
   return (
-    <>
+    <Card>
       <Test />
-    </>
+    </Card>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif',
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
