c5cebb32dfd7ee32e94bd656071bce8ddbe808b8
ChanGyunLee <lgyn10@naver.com>
2024-07-02 16:00:39 +0900

    chore: .

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,4 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       index: resolve(__dirname, 'index.html'),
-  //       popup: resolve(__dirname, 'public/popup.html'),
-  //       background: resolve(__dirname, 'public/background.ts'),
-  //     },
-  //     output: {
-  //       entryFileNames: '[name].js',
-  //       chunkFileNames: '[name].js',
-  //       assetFileNames: '[name].[ext]',
-  //     },
-  //   },
-  // },
 });
