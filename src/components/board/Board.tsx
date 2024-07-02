ce03af427782e40492a6a97cd101dd9c7e67ce7e
ChanGyunLee <lgyn10@naver.com>
2024-07-02 22:45:16 +0900

    feat: Container 안에 Board 컴포넌트들 구현

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,5 +1,25 @@
-const Board = () => {
-  return <div>Board</div>;
+import { InfoOutlineIcon } from '@chakra-ui/icons';
+import { Card, CardHeader, Heading, List, ListIcon, ListItem } from '@chakra-ui/react';
+
+const Board = (props: { title: string }) => {
+  const itemArr = ['item1', '아이템2', 'item3', 'item4', 'item', 'item', 'item', 'item', 'item'];
+  return (
+    <Card borderRadius='8px' bg='#dddddd'>
+      <CardHeader>
+        <Heading size='xm' textAlign='center'>
+          {props.title}
+        </Heading>
+      </CardHeader>
+      <List>
+        {itemArr.map((item, idx) => (
+          <ListItem key={idx}>
+            <ListIcon as={InfoOutlineIcon} />
+            {item}
+          </ListItem>
+        ))}
+      </List>
+    </Card>
+  );
 };
 
 export default Board;
