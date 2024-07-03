c71dacfc681e2f502c26bc97ba01b21a4046406e
ChanGyunLee <lgyn10@naver.com>
2024-07-03 10:16:18 +0900

    feat: Item 컴포넌트 코드 구현 및 스타일링

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,25 +1,33 @@
-import { InfoOutlineIcon } from '@chakra-ui/icons';
-import { Card, CardHeader, Heading, List, ListIcon, ListItem } from '@chakra-ui/react';
+import { Card, CardHeader, Heading, List } from '@chakra-ui/react';
+import styled from 'styled-components';
+import Item from '../item/Item';
 
 const Board = (props: { title: string }) => {
   const itemArr = ['item1', '아이템2', 'item3', 'item4', 'item', 'item', 'item', 'item', 'item'];
   return (
     <Card borderRadius='8px' bg='#dddddd'>
-      <CardHeader>
+      <StyledCardHeader>
         <Heading size='xm' textAlign='center'>
           {props.title}
         </Heading>
-      </CardHeader>
-      <List>
+      </StyledCardHeader>
+      <StyledList>
         {itemArr.map((item, idx) => (
-          <ListItem key={idx}>
-            <ListIcon as={InfoOutlineIcon} />
-            {item}
-          </ListItem>
+          <Item item={item} key={idx} />
         ))}
-      </List>
+      </StyledList>
     </Card>
   );
 };
 
 export default Board;
+
+const StyledCardHeader = styled(CardHeader)`
+  &&& {
+    padding: 0.5rem;
+  }
+`;
+
+const StyledList = styled(List)`
+  margin-bottom: 0.5rem;
+`;
--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,5 +1,44 @@
-const Item = () => {
-  return <div>Item</div>;
+import { EditIcon, InfoOutlineIcon } from '@chakra-ui/icons';
+import { Box, Button, ListIcon, ListItem } from '@chakra-ui/react';
+import styled from 'styled-components';
+
+const Item = (props: { item: string }) => {
+  return (
+    <StyledListItem>
+      <StyledBox>
+        <ListIcon as={InfoOutlineIcon} />
+        {props.item}
+      </StyledBox>
+      <StyledBox>
+        <StyledButton as={EditIcon}>...</StyledButton>
+      </StyledBox>
+    </StyledListItem>
+  );
 };
 
 export default Item;
+
+const StyledListItem = styled(ListItem)`
+  &&& {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-inline: 0.5rem;
+    width: 100%;
+  }
+`;
+
+const StyledBox = styled(Box)`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledButton = styled(Button)`
+  &&& {
+    height: max-content;
+    margin: 0;
+  }
+  :hover {
+    color: red;
+  }
+`;
