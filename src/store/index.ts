c3ebdf0ec0348b87c13420e4cbb2bc7a211829ea
ChanGyunLee <lgyn10@naver.com>
2024-07-07 21:02:08 +0900

    feat: 빌드용 dev용 데이터 할당

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,26 +4,16 @@ import { BookmarkState } from './types';
 
 // id = '2'인 기타 북마크 폴더만 가지고 crud 진행 예정
 export const bookmarkStore = create<BookmarkState>((set) => ({
-  bookmarkNode: defaultData,
+  bookmarkNode: null,
   fetchBookmarkTreeNode: async () => {
     try {
       const result = await chrome.bookmarks.getSubTree('2');
+      console.log('크롬에서 북마크 데이터를 가져옵니다.');
       set({ bookmarkNode: result[0] });
-
-      //
-      //| "permissions": ["bookmarks", "scripting", "activeTab"],
-      // const textData = JSON.stringify(result[0], null, 2);
-      // const blob = new Blob([textData], { type: 'text/plain' });
-      // const url = URL.createObjectURL(blob);
-
-      // const link = document.createElement('a');
-      // link.href = url;
-      // link.download = 'bookmarkNode.txt';
-      // document.body.appendChild(link);
-      // link.click();
-      // document.body.removeChild(link);
     } catch (error) {
       console.log('error', error);
+      console.log('로컬에서 북마크 데이터를 가져옵니다.');
+      set({ bookmarkNode: defaultData });
     }
   },
 }));
