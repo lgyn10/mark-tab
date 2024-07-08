9e86bf95be95e9737a8c5dcf660e5fa49fd27a12
ChanGyunLee <lgyn10@naver.com>
2024-07-08 16:34:05 +0900

    fix: 타입 선언 관련

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -10,7 +10,7 @@ type TBoardProps = {
 
 const Board = ({ boardTitle, boardId }: TBoardProps) => {
   const { bookmarkNode } = bookmarkStore();
-  const board = bookmarkNode?.children!.find((node) => node.id === boardId)?.children!;
+  const items = bookmarkNode?.children!.find((node) => node.id === boardId)?.children;
   return (
     <StyledCard>
       <StyledCardHeader>
@@ -19,9 +19,8 @@ const Board = ({ boardTitle, boardId }: TBoardProps) => {
         </Heading>
       </StyledCardHeader>
       <StyledList>
-        {board.map((node) => (
-          <Item key={node.id} itemTitle={node.title} itemUrl={node.url!} itemId={node.id} />
-        ))}
+        {items &&
+          items.map((node) => <Item key={node.id} itemTitle={node.title} itemUrl={node.url!} itemId={node.id} />)}
       </StyledList>
     </StyledCard>
   );
